dcd326a609ce15a9220f4eeee06330a0e21cdcda
Alamin <alamin8740@gmail.com>
2022-08-07 16:32:10 +0600

    create 2nd module burger_api

---
+++ burger_api/src/main/java/com/burger/burger_api/BurgerApiApplication.java
@@ -0,0 +1,13 @@
+package com.burger.burger_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgerApiApplication.class, args);
+	}
+
+}
---
+++ burger_api/src/test/java/com/burger/burger_api/BurgerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burger.burger_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
